c4849eaf8d61b5e5f6adf1e199b11fe6f327df66
Darya Rozhnova <hxrvelle@gmail.com>
2023-05-27 00:57:30 +0300

    Get img

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,16 @@
 package org.example;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Calendar;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
@@ -27,14 +30,20 @@ public class Main {
             System.out.println("Курс на вчера: " + yesterdayRub);
 
             if (todayRub > yesterdayRub) {
+                String compare = "More";
                 System.out.println("Больше");
+                getImg(compare);
             } else {
+                String compare = "Less";
                 System.out.println("Меньше");
+                getImg(compare);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
+        } catch (MalformedURLException ex) {
+            throw new RuntimeException(ex);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        } catch (ParseException ex) {
+            throw new RuntimeException(ex);
         }
     }
     static double getRub(String date) throws IOException, ParseException {
@@ -59,5 +68,33 @@ public class Main {
         jsonObject = (JSONObject) jsonParser.parse(rates);
         double rub = (double) jsonObject.get("RUB");
         return rub;
+    }
+
+    static String getImg(String compare) throws IOException, ParseException {
+        String apiKey = "prOAefWSeU2ktKrE8TL4W8QkUFGWKDZ5";
+        String endpoint = "broke";
+        if (compare.equals("More")) {
+            endpoint = "rich";
+        }
+        String path = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + endpoint;
+        URL url = new URL(path);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        InputStream is = connection.getInputStream();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+        String line;
+        StringBuilder result = new StringBuilder();
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = (JSONObject) jsonParser.parse(result.toString());
+        JSONArray jsonArray = (JSONArray) jsonObject.get("data");
+        int dataLength = jsonArray.size();
+        Random random = new Random();
+        int i = random.nextInt(0, dataLength);
+        JSONObject data = (JSONObject) jsonArray.get(i);
+        String img = data.get("url").toString();
+        System.out.println(img);
+        return img;
     }
 }
